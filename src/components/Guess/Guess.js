94ff1f0531702ed882491727c7a6733338049fe4
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2024-05-22 11:27:14 +0200

    correct 4 exercise solution

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,21 +3,16 @@ import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
 function Guess({ answer, value }) {
-  const word = checkGuess(value, answer);
-
-  function getClassName(index) {
-    let result = "cell";
-    if (word && word[index].status !== "incorrect") {
-      result += ` ${word[index].status}`;
-    }
-    return result;
-  }
+  const result = checkGuess(value, answer);
 
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className={getClassName(num)}>
-          {word ? word[num].letter : undefined}
+        <span
+          key={num}
+          className={`cell ${result ? result[num].status : undefined}`}
+        >
+          {result ? result[num].letter : undefined}
         </span>
       ))}
     </p>
